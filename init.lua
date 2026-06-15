2461e6b9795a087006de085261a762c86a1e69b3
micheal <keinesmicheal@gmail.com>
2026-06-16 00:18:28 +0200

    added tabstop

--- init.lua
+++ init.lua
@@ -1,1 +1,6 @@
 require("config.lazy")
+
+vim.opt.tabstop     = 4   -- tab = 4 spaces wide
+vim.opt.shiftwidth  = 4   -- indent = 4 spaces
+vim.opt.expandtab   = true -- convert tabs to spaces
+vim.opt.autoindent  = true -- keep indent on new line
